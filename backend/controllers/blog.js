dd7fb1eacdfdb38c21a75d5efb53104b2a3a75df
Oleksandr Pylypenko <lex.pilipenko@gmail.com>
2022-09-18 19:20:39 +0300

    complete classwork

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -18,8 +18,31 @@ blogRouter.get('/:id', async (request, response) => {
 blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  const savedBlogPost = await blog.save()
-  return response.status(201).json(savedBlogPost)
+  if (!request.body.title && !request.body.url) {
+    return response.status(400).json({ error: 'title and url must be provided'})
+  } else {
+    const savedBlogPost = await blog.save()
+    return response.status(201).json(savedBlogPost)
+  }
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+  const blogPost = await Blog.findById(request.params.id)
+
+  blogPost.likes = body.likes
+
+  try {
+    const updatedBlogPost = await blogPost.save()
+    return response.status(200).json(updatedBlogPost)
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogRouter
--- backend/tests/blog.test.js
+++ backend/tests/blog.test.js
@@ -15,59 +15,94 @@ beforeEach(async () => {
   }
 })
 
-test('blog posts are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('generic tests', () => {
+  test('blog posts are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 })
 
-test('all blog posts are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogPosts.length)
+describe('create', () => {
+  test('blog post is submitted', async () => {
+    const newBlogPost = {
+      author: 'Willy Wonka',
+      url: 'https://goatse.cx',
+      title: 'A tour around the Chocolate Factory',
+      likes: 0
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogPost)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogPostsAtEnd = await helper.blogPostsInDb()
+    expect(blogPostsAtEnd).toHaveLength(helper.initialBlogPosts.length + 1)
+
+    const posts = blogPostsAtEnd.map(x => x.title)
+    expect(posts).toContain(
+      'A tour around the Chocolate Factory'
+    )
+  })
+
+  test('likes propery set to 0 if not defined', async () => {
+    const newBlogPost = {
+      author: 'Willy Wonka',
+      url: 'https://goatse.cx',
+      title: 'A tour around the Chocolate Factory',
+    }
+
+    const savedBlogPost = await api
+      .post('/api/blogs')
+      .send(newBlogPost)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(+savedBlogPost.body.likes).toBe(0)
+  })
+
+  test('post is not saved if url or title are not provided', async () => {
+
+    await api
+      .post('/api/blogs')
+      .send({ author: 'Willy Wonka' })
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
 })
 
-test('name of ID property is correct', async () => {
-  const response = await api.get(`/api/blogs/${helper.initialBlogPosts[0]._id}`)
-  // console.log(helper.initialBlogPosts[0]._id, response)
-  expect(response.body.id).toBeDefined()
+describe('read', () => {
+  test('all blog posts are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogPosts.length)
+  })
+
+  test('name of ID property is correct', async () => {
+    const response = await api.get(`/api/blogs/${helper.initialBlogPosts[0]._id}`)
+    expect(response.body.id).toBeDefined()
+  })
 })
 
-test('blog post is submitted', async () => {
-  const newBlogPost = {
-    author: 'Willy Wonka',
-    url: 'https://goatse.cx',
-    title: 'A tour around the Chocolate Factory',
-    likes: 0
-  }
+describe('delete', () => {
+  test('post can be deleted', async () => {
 
-  await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogPostsAtEnd = await helper.blogPostsInDb()
-  expect(blogPostsAtEnd).toHaveLength(helper.initialBlogPosts.length + 1)
-
-  const posts = blogPostsAtEnd.map(x => x.title)
-  expect(posts).toContain(
-    'A tour around the Chocolate Factory'
-  )
+    await api
+      .delete(`/api/blogs/${helper.initialBlogPosts[0]._id}`)
+      .expect(204)
+  })
 })
 
-test('"likes" propery set to 0 if not defined', async () => {
-  const newBlogPost = {
-    author: 'Willy Wonka',
-    url: 'https://goatse.cx',
-    title: 'A tour around the Chocolate Factory',
-  }
+describe('put', () => {
+  test('likes are updated', async () => {
+    const response = await api
+      .put(`/api/blogs/${helper.initialBlogPosts[0]._id}`)
+      .send({ likes: 100 })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  const savedBlogPost = await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  expect(+savedBlogPost.body.likes).toBe(0)
+    expect(response.body.likes).toBe(100)
+  })
 })
